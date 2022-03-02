8753080135c4da11101e77dd3ef176b525efd902
Alisimran95 <alisimranem@code.edu.az>
2022-03-02 23:34:41 +0400

    homework

---
+++ P320Practise/P320Practise/P320Practise/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P320FrontToBack.Areas.AdminPanel.Data;
+using P320Practise.Areas.AdminPanel.Data;
+using P320Practise.DataAccessLayer;
+using P320Practise.Models;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace P320Practise.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CategoryController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _dbContext.Categories.Where(x => x.IsDeleted == false)
+                .ToListAsync();
+
+            return View(categories);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var parentCategories = await _dbContext.Categories
+                .Where(x => x.IsDeleted == false && x.IsMain).ToListAsync();
+            ViewBag.ParentCategories = parentCategories;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category, int parentCategoryId)
+        {
+            var parentCategories = await _dbContext.Categories
+                .Where(x => x.IsDeleted == false && x.IsMain).ToListAsync();
+            ViewBag.ParentCategories = parentCategories;
+
+            if (!ModelState.IsValid)
+                return View();
+
+            if (category.IsMain)
+            {
+                if (category.Photo == null)
+                {
+                    ModelState.AddModelError("", "Shekil sechin.");
+                    return View();
+                }
+
+                if (!category.Photo.IsImage())
+                {
+                    ModelState.AddModelError("", "Duzgun shekil formati sechin.");
+                    return View();
+                }
+
+                if (!category.Photo.IsAllowedSize(1))
+                {
+                    ModelState.AddModelError("", "1Mb-dan artiq ola bilmez.");
+                    return View();
+                }
+
+                var fileName = await category.Photo.GenerateFile(Constants.ImageFolderPath);
+
+                category.Image = fileName;
+            }
+            else
+            {
+                if (parentCategoryId == 0)
+                {
+                    ModelState.AddModelError("", "Parent kateqoriyasi sechin.");
+                    return View();
+                }
+
+                var existParentCategory = await _dbContext.Categories
+                    .Include(x => x.Children.Where(y => y.IsDeleted == false))
+                    .FirstOrDefaultAsync(x => x.IsDeleted == false && x.Id == parentCategoryId);
+                if (existParentCategory == null)
+                    return NotFound();
+
+                var existChildCategory = existParentCategory.Children
+                    .Any(x => x.Name.ToLower() == category.Name.ToLower());
+                if (existChildCategory)
+                {
+                    ModelState.AddModelError("", "Bu adda kateqoriya var.");
+                    return View();
+                }
+
+                category.Parent = existParentCategory;
+            }
+
+            category.IsDeleted = false;
+            await _dbContext.Categories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var category = await _dbContext.Categories
+                .Where(x => x.Id == id && x.IsDeleted == false)
+                .Include(x => x.Parent)
+                .Include(x => x.Children.Where(y => y.IsDeleted == false))
+                .FirstOrDefaultAsync();
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteCategory(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var category = await _dbContext.Categories
+                .Where(x => x.Id == id && x.IsDeleted == false)
+                .Include(x => x.Children.Where(y => y.IsDeleted == false))
+                .FirstOrDefaultAsync();
+            if (category == null)
+                return NotFound();
+
+            category.IsDeleted = true;
+            if (category.IsMain)
+            {
+                foreach (var item in category.Children)
+                {
+                    item.IsDeleted = true;
+                }
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var category = await _dbContext.Categories
+                .Where(x => x.Id == id && x.IsDeleted == false)
+                .Include(x => x.Parent)
+                .Include(x => x.Children.Where(y => y.IsDeleted == false))
+                .FirstOrDefaultAsync();
+
+            return View(category);
+        }
+
+        public async Task<IActionResult> Update()
+        {
+            var parentCategories = await _dbContext.Categories
+                .Where(x => x.IsDeleted == false && x.IsMain).ToListAsync();
+            ViewBag.ParentCategories = parentCategories;
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(Category category, int? parentCategoryId,int? id)
+        {
+
+            var parentCategories = await _dbContext.Categories
+                .Where(x => x.IsDeleted == false && x.IsMain).ToListAsync();
+                 ViewBag.ParentCategories = parentCategories;
+
+            var existCategory = await _dbContext.Categories
+                .Where(x => x.Id == id && x.IsDeleted==false)
+                .Include(x=>x.Parent)
+                .Include(x=>x.Children.Where(y=>y.IsDeleted == false))
+                .FirstOrDefaultAsync();
+
+            if (id==null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+                return View();
+
+            if (category.IsMain)
+            {
+                if (category.Photo == null)
+                {
+                    ModelState.AddModelError("", "Shekil sechin.");
+                    return View();
+                }
+
+                if (!category.Photo.IsImage())
+                {
+                    ModelState.AddModelError("", "Duzgun shekil formati sechin.");
+                    return View();
+                }
+
+                if (!category.Photo.IsAllowedSize(1))
+                {
+                    ModelState.AddModelError("", "1Mb-dan artiq ola bilmez.");
+                    return View();
+                }
+
+                var fileName = await category.Photo.GenerateFile(Constants.ImageFolderPath);
+
+                category.Image = fileName;
+            }
+            else
+            {
+                if (parentCategoryId == 0)
+                {
+                    ModelState.AddModelError("", "Parent kateqoriyasi sechin.");
+                    return View();
+                }
+
+                var existParentCategory = await _dbContext.Categories
+                    .Include(x => x.Children.Where(y => y.IsDeleted == false))
+                    .FirstOrDefaultAsync(x => x.IsDeleted == false && x.Id == parentCategoryId);
+                if (existParentCategory == null)
+                    return NotFound();
+
+                var existChildCategory = existParentCategory.Children
+                    .Any(x => x.Name.ToLower() == category.Name.ToLower());
+                if (existChildCategory)
+                {
+                    ModelState.AddModelError("", "Bu adda kateqoriya var.");
+                    return View();
+                }
+
+                category.Parent = existParentCategory;
+            }
+
+            existCategory.Name = category.Name;
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
